8b01b86bf130104398471448e742913d263e8916
kobanori <n.koppan@gmail.com>
2020-09-01 23:53:50 +0900

    商品画像投稿機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,6 @@ class ItemsController < ApplicationController
   end
   
   def item_params
-    params.require(:item).permit(:name,:description,:category_id,:condition_id,:postage_id,:prefecture_id,:set_up_time_id,:price)
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :set_up_time_id, :price).merge(user_id: current_user.id)
   end
 end
