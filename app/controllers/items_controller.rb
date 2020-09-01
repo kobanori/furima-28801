ecb95c676d78b9dcff99dc58242fa83fee84207e
kobanori <n.koppan@gmail.com>
2020-09-01 19:24:59 +0900

    出品ページへの遷移の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :move_to_sign_up, except: [:index, :show]
   def index
+  end
+
+  private
+  def move_to_sign_up
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'items/index'
+  resources :items, only: [:index, :new]
   root to: "items#index"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
