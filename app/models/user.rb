744865a8f89ac179275e46daaf30a4f44ee65587
kobanori <n.koppan@gmail.com>
2020-09-01 19:21:10 +0900

    itemsテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
   validates :password, length: { minimum: 6 }, format: { with: /[a-z]{1,}[\d]{1,}/, message: 'include both letters and numbers' }
   with_options presence: true do
     validates :nickname
