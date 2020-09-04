741b423640c9e788baff951766015038cacc2870
kobanori <n.koppan@gmail.com>
2020-09-04 20:45:57 +0900

    不要な記述の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_sign_up, except: [:index, :show, :edit]
-  before_action :set_item, only: [:create, :show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_item_show, only: [:edit]
   def index
   end
