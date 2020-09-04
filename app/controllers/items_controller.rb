9633ca2cc6c113ad7c3499e98aca35f746bd5025
kobanori <n.koppan@gmail.com>
2020-09-04 16:01:15 +0900

    Rubocopチェックによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,9 +27,11 @@ class ItemsController < ApplicationController
       redirect_to new_user_session_path
     end
   end
+
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :set_up_time_id, :price).merge(user_id: current_user.id)
   end
+  
   def set_item
     @item = Item.find(params[:id])
   end
