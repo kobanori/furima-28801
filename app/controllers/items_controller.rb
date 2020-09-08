bf4e0d5cbe9c8d6dd89186e738aab564bf8ee164
kobanori <n.koppan@gmail.com>
2020-09-08 21:04:59 +0900

    商品一覧機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_item_show, only: [:edit]
   def index
+    @items = Item.all
   end
 
   def new
