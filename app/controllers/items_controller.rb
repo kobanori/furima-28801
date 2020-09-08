c264de895d16f5523e37cde0725c4edcc144b62b
kobanori <n.koppan@gmail.com>
2020-09-08 21:32:24 +0900

    商品表示順の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_item_show, only: [:edit]
   def index
-    @items = Item.all
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
