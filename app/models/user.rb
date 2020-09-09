ad3c7e9e4e6bd1c2798ea6cc37d0e45d973199bb
kobanori <n.koppan@gmail.com>
2020-09-10 00:23:43 +0900

    日本語化ファイルの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :items
   has_many :orders
-  validates :password, length: { minimum: 6 }, format: { with: /[a-z]{1,}[\d]{1,}/, message: 'include both letters and numbers' }
+  validates :password, length: { minimum: 6 }, format: { with: /[a-z]{1,}[\d]{1,}/, message: 'は半角英数をそれぞれ1文字以上使用してください' }
   with_options presence: true do
     validates :nickname
     validates :birthday
   end
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角文字で入力してください(全角英数は除く)' } do
     validates :first_name
     validates :last_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角カナで入力してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
